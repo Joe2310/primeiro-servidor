68170d3d05b0b91ef04ba2e30c4f2ff8592be87e
joe silva <joe.ssa@hotmail.com>
2023-08-04 16:34:10 -0300

    Primeiro_servidor

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+
+let usuarios = [
+    {id: 1, nome: 'joe', idade: 24, profissao:'FULL STACK'},
+    {id: 1, nome: 'Joao', idade: 35, profissao:'Mecanico'},
+    {id: 1, nome: 'Gilvania', idade: 19, profissao:'Designer'},
+    {id: 1, nome: 'Marcos', idade: 28, profissao:'Vaqueiro'}
+
+]
+    
+
+
+
+
+app.get('/',(req, res)=> {
+    res.send('Essa é minha rota principal')
+
+})
+
+
+
+
+
+app.get('/usuarios',(req, res)=> {
+    res.send(usuarios)
+})
+
+
+app.listen(3000);
